03544d61a58b8e66e801d1b8703cd43263717a6f
Sean Dhillon <sean.dhill2004@gmail.com>
2025-05-07 01:23:22 -0700

    Band Name Generator Text Version

---
+++ band-name-gen/main.py
@@ -0,0 +1,10 @@
+print("Welcome to the Band Name Generator!\n")
+
+city = input("What city did you grow up in?\n")
+
+pet_name = input("What is your pets name?\n")
+
+band_name = city + " " + pet_name
+
+print ("Your band name could be " + band_name)
+
