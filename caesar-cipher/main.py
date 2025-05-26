41c9c342ba70b5ef1b70d5aaee1971e5c96aea83
Sean Dhillon <sean.dhill2004@gmail.com>
2025-05-26 15:47:04 -0700

    Added Decryption Logic

--- caesar-cipher/main.py
+++ caesar-cipher/main.py
@@ -9,6 +9,18 @@ def encrypt(original_text, shift_amount):
             new_string += char
     print(f"Here is your encrypted result: {new_string} ")
 
+def decrypt(original_text, shift_amount):
+    decrypted_string = ''
+    for char in original_text:
+        if char in alphabet:
+            index_in_alphabet = alphabet.index(char)
+            shifted_index = index_in_alphabet - shift % 26
+            decrypted_string += alphabet[shifted_index]
+        else:
+            decrypted_string += char
+        
+    print(f"Here is your decrypted result: {decrypted_string} ")
+
 
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
@@ -22,3 +34,5 @@ shift = int(input("Type the shift number:\n"))
 
 if direction == 'encode':
     encrypt(text, shift)
+if direction == 'decode':
+    decrypt(text, shift)
