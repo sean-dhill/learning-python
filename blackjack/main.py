61e9f7dba17eb2a830e0759c76cdcd84c1661a46
Sean Dhillon <sean.dhill2004@gmail.com>
2025-06-01 23:16:02 -0700

    Added functionality to allow for multiple player cards to be drawn

--- blackjack/main.py
+++ blackjack/main.py
@@ -11,13 +11,28 @@ logo = r"""
       `------'                           |__/           
 """
 playing_cards = [11,2,3,4,5,6,7,8,9,10,10,10]
+game_on = True
+while game_on:
+    print(logo)
 
-print(logo)
-
-player_cards = [playing_cards[random.randint(0, len(playing_cards) - 1)],playing_cards[random.randint(0, len(playing_cards) - 1)]]
-print(f"Your cards: {player_cards}, current score: {sum(player_cards)}")
+    player_cards = [playing_cards[random.randint(0, len(playing_cards) - 1)],playing_cards[random.randint(0, len(playing_cards) - 1)]]
+    print(f"Your cards: {player_cards}, current score: {sum(player_cards)}")
     
-computer_cards = [playing_cards[random.randint(0, len(playing_cards) - 1)]]
-print(f"Computer's first card: {computer_cards}")
+    computer_cards = [playing_cards[random.randint(0, len(playing_cards) - 1)]]
+    print(f"Computer's first card: {computer_cards}")
      
+    new_card = input("Type 'y' to get another card, type 'n' to pass: ").lower()
+    while new_card not in ('y','n'):
+        new_card = input("Please enter either 'y' or 'n': ")
     
+    if new_card == 'y':
+        while new_card == 'y':
+            player_cards.append(playing_cards[random.randint(0, len(playing_cards) - 1)])
+            print(f"Your cards: {player_cards}, current score: {sum(player_cards)}")
+            print(f"Computer's first card: {computer_cards}")
+            if sum(player_cards) > 21:
+                print("Game over, you went over 21!")
+                game_on = False
+                break
+            new_card = input("Type 'y' to get another card, type 'n' to pass: ")
+
